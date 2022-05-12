18241c9ed10486cf4ff199ed7f6f7e2284839702
LuizFabiodoCarmo <104438927+LuizFabiodoCarmo@users.noreply.github.com>
2022-05-12 10:53:19 -0300

    testando pastas

---
+++ Bloco 1/Java/src/laçosDeDecisao/TesteDepastas.java
@@ -0,0 +1,11 @@
+
+public class TesteDepastas {
+
+
+	public static void main(String[] args) {
+	 double salario = 1300.80;
+	 int valor = (int) salario;
+	 //O (int) declara pro java que sei que estarei perdendo valor na conversão de real pra inteiro.//
+	 System.out.println(valor);
+	} 
+}
